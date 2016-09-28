d1e0fb69d353b0429aeaf619e987355f63dd1a87
Stef Collart <stefcollart@hotmail.com>
2016-09-28 17:54:53 +0200

    test + begin herhalingsoefening

---
+++ 0. Herhaling/Oefening1/Datum.java
@@ -0,0 +1,12 @@
+package Oefening1;
+
+public class Datum {
+
+	private int dag, maand, jaar;
+	
+	public Datum(int dag, int maand, int jaar) {
+		this.dag = dag;
+		this.maand = maand;
+		this.jaar = jaar;
+	}
+}
