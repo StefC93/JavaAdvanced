1a723b24b6ae4074873c79e17df2c3acfbe15d2c
Stef Collart <stefcollart@hotmail.com>
2016-09-28 18:08:51 +0200

    aaa

---
+++ 0. Herhaling/Oefening1/Werknemer.java
@@ -0,0 +1,13 @@
+package Oefening1;
+
+public class Werknemer {
+	private String nr, naam; 
+	private Datum datumInD;
+	
+	public Werknemer(String nr, String naam, Datum datumInD) {
+		this.nr = nr;
+		this.naam = naam;
+		this.datumInD = datumInD;
+	}
+
+}
