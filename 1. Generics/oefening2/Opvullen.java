b7e86b5f6d2241cc356b510b4bbbdebdd50f0e2d
Stef Collart <stefcollart@hotmail.com>
2016-10-04 20:22:18 +0200

    Oefening2 voltooid

---
+++ 1. Generics/oefening2/Opvullen.java
@@ -0,0 +1,29 @@
+package oefening2;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Opvullen<T> {
+	private T vulling;
+
+	public T opvullen() {
+		return vulling;
+	}
+
+	public Opvullen(T vulling) {
+		this.vulling = vulling;
+	}
+
+	public static <T> void main(String[] args) {
+		List<Opvullen> myList = new ArrayList<>();
+		
+		for (int i = 0; i < 20; i++) {
+			myList.add(new Opvullen<String>("nummer: " + (i+1)));
+		}
+		
+		for (Opvullen item : myList){
+			System.out.println(item.vulling);
+		}
+	}
+
+}
