6eb12638b361c1a93370f09ac2f49ad3fdfd4ec6
Stef Collart <stefcollart@hotmail.com>
2017-01-18 12:31:57 +0100

    opdracht 2

--- 3. Collections/opdracht2/Persoon.java
+++ 3. Collections/opdracht2/Persoon.java
@@ -77,4 +77,53 @@ public class Persoon {
 		this.height = height;
 	}
 
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + age;
+		result = prime * result + ((firstName == null) ? 0 : firstName.hashCode());
+		result = prime * result + ((gender == null) ? 0 : gender.hashCode());
+		long temp;
+		temp = Double.doubleToLongBits(height);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		result = prime * result + ((lastName == null) ? 0 : lastName.hashCode());
+		temp = Double.doubleToLongBits(weight);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Persoon other = (Persoon) obj;
+		if (age != other.age)
+			return false;
+		if (firstName == null) {
+			if (other.firstName != null)
+				return false;
+		} else if (!firstName.equals(other.firstName))
+			return false;
+		if (gender == null) {
+			if (other.gender != null)
+				return false;
+		} else if (!gender.equals(other.gender))
+			return false;
+		if (Double.doubleToLongBits(height) != Double.doubleToLongBits(other.height))
+			return false;
+		if (lastName == null) {
+			if (other.lastName != null)
+				return false;
+		} else if (!lastName.equals(other.lastName))
+			return false;
+		if (Double.doubleToLongBits(weight) != Double.doubleToLongBits(other.weight))
+			return false;
+		return true;
+	}
+
 }
--- 3. Collections/opdracht2/PersoonApp.java
+++ 3. Collections/opdracht2/PersoonApp.java
@@ -6,21 +6,23 @@ import java.util.Set;
 public class PersoonApp {
 
 	public static void main(String[] args) {
-		
+
 		Set<Persoon> personen = new HashSet<>();
-		
+
 		Persoon p1 = new Persoon("Collart", "Stef", "Man", 23, 72, 1.80);
 		Persoon p2 = new Persoon("Jorissen", "Veerle", "Vrouw", 59, 70, 1.70);
-		
+
 		Persoon p1punt1 = new Persoon("Collart", "Stef", "Man", 23, 72, 1.80);
-		
+
 		personen.add(p1);
 		personen.add(p2);
 		personen.add(p1punt1);
-		
-		for (Persoon el:personen){
-			System.out.println(el);
-		}
+
+
+		/*
+		 * for (Persoon el:personen){ System.out.println(el); }
+		 */
+		personen.forEach(s -> System.out.println(s));
 	}
 
 }
