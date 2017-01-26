1b0fa13a9075d94c2d395f69718bf3cea292b849
Stef Collart <stefcollart@hotmail.com>
2017-01-26 16:06:37 +0100

    Minor change

--- Algemene Oefeningen/callLogs/CallLogApp.java
+++ Algemene Oefeningen/callLogs/CallLogApp.java
@@ -1,20 +1,20 @@
 package callLogs;
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Set;
 import java.util.TreeSet;
-import java.util.concurrent.ArrayBlockingQueue;
 
 public class CallLogApp {
 
 	public static void main(String[] args) {
 		Set<StringBuffer> set = new TreeSet();
 
-		ArrayList<String> logs = new ArrayList<String>();
+		List<String> logs = new ArrayList<String>();
 		String[] lines = CallLogData.getCallLogData().split("\n");
 
 		for (String log : lines) {
-			// System.out.println(log);
+			//System.out.println(log);
 			logs.add(log);
 		}
 		
